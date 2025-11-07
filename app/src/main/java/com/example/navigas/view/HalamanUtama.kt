523d540dad9ef7f5c6f0f349344a262a7d063183
MuhammadShidqulIman <muhammadshidqul3@gmail.com>
2025-11-07 16:04:54 +0700

    Tampilan sambutan pengguna di halaman utama

---
+++ app/src/main/java/com/example/navigas/view/HalamanUtama.kt
@@ -0,0 +1,32 @@
+package com.example.navigas.view
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.example.navigas.R
+
+@Composable
+fun HalamanUtama(navController: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            "Selamat Datang",
+            fontSize = 30.sp,
+            color = Color(0xFF800080),
+            fontWeight = FontWeight.Bold
+        )
