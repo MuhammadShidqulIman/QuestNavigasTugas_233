6ca77b60f084d392b2a346179e109c03c24623c0
MuhammadShidqulIman <muhammadshidqul3@gmail.com>
2025-11-07 16:10:07 +0700

    `Tambah FormulirScreen dengan input dan tombol Submit`

---
+++ app/src/main/java/com/example/navigas/view/Formulir.kt
@@ -0,0 +1,23 @@
+package com.example.navigas.view
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+
+data class Formulir(
+    val nama: String,
+    val kelamin: String,
+    val status: String,
+    val alamat: String
+)
